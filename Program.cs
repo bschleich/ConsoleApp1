b16bdcb358e635187c453781e604170451c8d36e
Byron Schleich <65250912+bschleich@users.noreply.github.com>
2020-05-19 20:44:42 -0400

    20200519HPa Update

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub and World");
+            Console.WriteLine("Hello GitHub, Clone Copy and World");
             Console.ReadKey();
         }
     }
