99824ac12c96fbceb79786c068013a83d1de961e
Byron Schleich <65250912+bschleich@users.noreply.github.com>
2020-05-27 14:24:03 -0400

    Update from HP

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub and World");
-            Console.WriteLine("Working with the master branch on VSC on MacOS...");
+            Console.WriteLine("Working with the master branch on the HP...");
             Console.ReadKey();
         }
     }
